30e526679efe6ae6b241093eda39dc837840c26e
wietskedotinga@hotmail.com <wietskedotinga@hotmail.com>
2016-11-02 15:03:58 +0100

    created objects.py

---
+++ objects.py
@@ -0,0 +1,14 @@
+# Cars and grid
+
+class carType:
+    def _init_(self,name):
+        self.name = name
+        if name == "RedCar":
+            self.size = 2
+        elif name == "Truck":
+            self.size = 3
+
+class grid:
+    cars = []
+    width = 6
+    height = 6
