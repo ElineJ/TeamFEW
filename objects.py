1947cd992e008f1a70731245253587c54a295ca4
fjodor-rs <fjodor_rs@hotmail.com>
2016-11-03 14:20:15 +0100

    Update objects.py

--- objects.py
+++ objects.py
@@ -1,14 +1,37 @@
 # Cars and grid
+import numpy as np
+class Position(obhject):
+    
 
-class carType:
-    def _init_(self,name):
+class VeGrid(object):
+
+    def __init__(self, width, height, exit):
+        self.width = width
+        self.height = height
+        self.exit = exit
+        grid = []
+        self.grid = grid
+        
+    def isRedOnExit(self, pos):
+        if pos == self.exit:
+            return True
+        return False
+    def isPositioninGrid(self, pos)
+        vehicle.x 
+
+
+class Vehicle:
+    
+    def __init__(self, position, grid):
+        self.grid = grid
         self.name = name
-        if name == "RedCar":
-            self.size = 2
-        elif name == "Truck":
-            self.size = 3
-
-class grid:
-    cars = []
-    width = 6
-    height = 6
+        self.pos = position
+    def getVehiclePosition(self):
+        return self.pos
+    def setVehiclePosition(self, position):
+        self pos = position
+class Hcar(Vehicle):
+    def __init__(self):
+        length = 3
+    def movement(self):
+        
