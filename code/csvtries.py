8f780d7718c5fa354122e276a1ec89fe1c965293
wietskedotinga@hotmail.com <wietskedotinga@hotmail.com>
2016-11-10 14:02:00 +0100

    changes to csvtries

--- code/csvtries.py
+++ code/csvtries.py
@@ -1,6 +1,28 @@
-import csv
+import csv     # imports the csv module
+import sys      # imports the sys module
 
-with open('game3.csv') as csvfile:
-    reader = csv.reader(csvfile)
-    for row in reader:
-         print row
+car_list = []
+
+class Car(object):
+    """
+    Creates an object for a vehicle type of car
+    """
+    def __init__(self, orientation, color):
+
+        self.orientation = orientation
+        self.color = color
+
+
+f = open(sys.argv[1], 'rb') # opens the csv file
+try:
+    reader = csv.reader(f)  # creates the reader object
+    for row in reader:   # iterates the rows of the file in orders
+        car_list.append(Car(row[3], row[4]))    # puts values in list
+finally:
+    f.close()      # closing
+
+print [Car.color for Car in car_list]
+
+# Code for the game rush hour
+#
+# Sets up vehicles (trucks and cars) and a grid for the game
