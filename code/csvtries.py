b267c72606dabee7266cbe0876fcd322b7600733
wietskedotinga@hotmail.com <wietskedotinga@hotmail.com>
2016-11-10 14:44:30 +0100

    csvtries reads cars no trucks

--- code/csvtries.py
+++ code/csvtries.py
@@ -16,13 +16,21 @@ class Car(object):
 f = open(sys.argv[1], 'rb') # opens the csv file
 try:
     reader = csv.reader(f)  # creates the reader object
+    # skip first line of csv file
+    next(reader)
     for row in reader:   # iterates the rows of the file in orders
+        # get string of x coordinates
+        str = row[1]
+        # split this string
+        list = str.split(", ")
+        # save values from list
+        x1 = list[0]
+        x2 = list[1]
+        # print "coordinates are x1 = " + x1 + x2
+
+        # add car with the values from csv file to the list of cars
         car_list.append(Car(row[3], row[4]))    # puts values in list
 finally:
     f.close()      # closing
 
 print [Car.color for Car in car_list]
-
-# Code for the game rush hour
-#
-# Sets up vehicles (trucks and cars) and a grid for the game
