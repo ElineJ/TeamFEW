61bc804f4772144e36b4a081bac33162a3ca8782
ElineJ <elinejacobse@gmail.com>
2016-11-21 09:52:21 +0100

    Updated visualize.py

--- code/visualize.py
+++ code/visualize.py
@@ -1,85 +1,89 @@
 # Visualization code for the game Rush hour
-#
-#
-
-import math
-import time
 
 from Tkinter import *
 import matplotlib
 matplotlib.use("TkAgg")
 from matplotlib import pyplot as plt
 
+all_vehicles = []
+
+class CarPosition(object):
+    """
+    A Position represents a position on the grid
+    """
+    def __init__(self, x1, x2, y1, y2):
+        """
+        Initializes a position with coordinates (x, x, y, y).
+        """
+        self.x1 = x1
+        self.x2 = x2
+        self.y1 = y1
+        self.y2 = y2
+    def getX(self):
+        x = [self.x1, self.x2]
+        return x
+    def getY(self):
+        y = [self.y1, self.y2]
+        return y
+
+class Car(object):
+    """
+    Creates an object for a vehicle type of car
+    """
+    def __init__(self, x1, x2, y1, y2, orientation, color):
+        self.orientation = orientation
+        self.color = color
+        self.x1 = x1
+        self.x2 = x2
+        self.y1 = y1
+        self.y2 = y2
+
+def makeCars():
+    car = Car(0, 0, 0, 1, 'V', 'blue')
+    all_vehicles.append(car)
+    car2 = Car(4, 5, 4, 4, 'H', 'blue')
+    all_vehicles.append(car2)
+    car3 = Car(1, 1, 1, 2, 'V', 'red')
+    all_vehicles.append(car3)
+
+
 class Visualization:
     def __init__(self, width, height, delay = 0.2):
-        "Initializes a visualization with the specified parameters."
+
         # Number of seconds to pause after each frame
         self.delay = delay
         self.max_dim = max(width, height)
         self.width = width
         self.height = height
+        self.rows = height
+        self.columns = width
+        self.cellwidth = 500/self.columns
+        self.cellheight = 500/self.rows
 
         # Initialize a drawing surface
         self.master = Tk()
         self.w = Canvas(self.master, width=500, height=500)
         self.w.pack()
         self.master.update()
+        self.master.title("Rush hour")
 
-        # Draw a backing and lines
-        x1, y1 = self._map_coords(0, 0)
-        x2, y2 = self._map_coords(width, height)
-        self.w.create_rectangle(x1, y1, x2, y2, fill = "white")
+        # draw a grid
+        self.rect = {}
+        for column in range(0, self.columns):
+            for row in range(0, self.rows):
+                x1 = column * self.cellwidth
+                y1 = row * self.cellheight
+                x2 = x1 + self.cellwidth
+                y2 = y1 + self.cellheight
+                self.rect[row, column] = self.w.create_rectangle(x1,y1,x2,y2, fill="#dbe8ff", tags="rect")
 
-        # Draw squares for the grid
-        self.grid = {}
-        for i in range(width):
-            for j in range(height):
-                x1, y1 = self._map_coords(i, j)
-                x2, y2 = self._map_coords(i + 1, j + 1)
-                self.grid[(i, j)] = self.w.create_rectangle(x1, y1, x2, y2,
-                                                             fill = "#dbe8ff")
-
-        # Draw
-
-        # Draw gridlines
-        for i in range(width + 1):
-            x1, y1 = self._map_coords(i, 0)
-            x2, y2 = self._map_coords(i, height)
-            self.w.create_line(x1, y1, x2, y2)
-        for i in range(height + 1):
-            x1, y1 = self._map_coords(0, i)
-            x2, y2 = self._map_coords(width, i)
-            self.w.create_line(x1, y1, x2, y2)
-
-        # draw shapes for cars
-        x1, y1 = self._map_coords(0, 0)
-        x2, y2 = self._map_coords(0, 1)
-        self.w.create_rectangle(x1, y1, x2, y2, fill="red")
-
-    def _draw_vehicles(self, position, direction):
-        "Returns a polygon representing a robot with the specified parameters."
-        x, y = position.getX(), position.getY()
-        d1 = direction + 165
-        d2 = direction - 165
-        x1, y1 = self._map_coords(x, y)
-        x2, y2 = self._map_coords(x + 0.6 * math.sin(math.radians(d1)),
-                                  y + 0.6 * math.cos(math.radians(d1)))
-        x3, y3 = self._map_coords(x + 0.6 * math.sin(math.radians(d2)),
-                                  y + 0.6 * math.cos(math.radians(d2)))
-        return self.w.create_polygon([x1, y1, x2, y2, x3, y3], fill="red")
-
-        # Draw some status text
-        #self.robots = None
-        #self.text = self.w.create_text(25, 0, anchor=NW,
-        #                               text=self._status_string(0, 0))
-        #self.time = 0
-        #self.master.update()
-
-    def _map_coords(self, x, y):
-        "Maps grid positions to window positions (in pixels)."
-        return (250 + 450 * ((x - self.width / 2.0) / self.max_dim),
-                250 + 450 * ((self.height / 2.0 - y) / self.max_dim))
-
-    def done(self):
-        "Indicate that the animation is done so that we allow the user to close the window."
-        mainloop()
+        # draw vehicles
+        for i in range(0, len(all_vehicles)):
+            x1, y1 = all_vehicles[i].x1 * self.cellwidth, all_vehicles[i].y1 * self.cellheight
+            if all_vehicles[i].orientation == 'V':
+                x2, y2 = x1 + self.cellwidth, y1 + 2 * self.cellheight
+            else:
+                x2, y2 = x1 + 2 * self.cellwidth , y1 + self.cellheight
+            self.rect[all_vehicles[1].x1, all_vehicles[i].y1] = self.w.create_rectangle(x1, y1, x2, y2, fill = all_vehicles[i].color)
+            print "x1, y1: ", x1, y1
+            print "x2, y2: ", x2, y2
