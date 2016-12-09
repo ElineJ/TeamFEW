caabb9df7d64936a9a3b0a858318d5667877bb21
ElineJ <elinejacobse@gmail.com>
2016-12-09 13:21:43 +0100

    Added test function and results for random

---
+++ code/test.py
@@ -0,0 +1,43 @@
+import sys
+
+import positions as pos
+import grid
+import csvtries
+import bfs
+import rndom as rd
+from copy import deepcopy
+
+import csv
+
+# open csv file
+f = open(sys.argv[1], 'rb')
+width = int(sys.argv[2])
+height = width
+num_runs = int(sys.argv[3])
+
+# check which exit to use
+if width == 6:
+    exit = pos.GridPosition(5, 2)
+elif width == 9:
+    exit = pos.GridPosition(8, 4)
+elif width == 12:
+    exit = pos.GridPosition(11, 5)
+
+# set up grid with vehicles
+grid_rnd = csvtries.run(f, width, height, exit)
+# grid_bfs = deepcopy(grid_rnd)
+f.close()
+
+for i in range (0, num_runs):
+    new_grid = deepcopy(grid_rnd)
+    # print "--- Random algoritme ---"
+    result = rd.runrandom(new_grid, exit)
+    # print result
+    # add result to csv file
+    with open('../results/random_7.csv', 'a') as testFile:
+        testFileWriter = csv.writer(testFile)
+        testFileWriter.writerow(result)
+
+print "done"
+#print "--- bfs ---"
+#bfs.runbfs(grid_bfs, exit)
