0066749e192afe609c66ec603bbb0586737408fb
ElineJ <elinejacobse@gmail.com>
2016-12-14 16:59:44 +0100

    Changed rndom.py

--- code/rndom.py
+++ code/rndom.py
@@ -43,14 +43,15 @@ def runrandom(grid, exit):
                         moves += 1
                         # anim.update(vehicles)
                         # check if car is at exit
-                        if vehicles[i].pos.x2 == exit.x and vehicles[i].pos.y2 == exit.y:
+                        if vehicles[i].pos.x2 == exit.x and
+                        vehicles[i].pos.y2 == exit.y:
                             # print "Found exit!"
                             # print "Moves:", moves
-                            # print("--- %s seconds ---" % (time.time() - start_time))
                             # del results[:]
                             results.append(moves)
                             results.append("%.6s" % (time.time() - start_time))
-                            # print moves,("%s seconds" % (time.time() - start_time))
+                            # print moves,("%s seconds" % (time.time() -
+                            # start_time))
                             return results
 
             elif isinstance(vehicles[i], truck.Truck):
